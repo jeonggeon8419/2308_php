6eba7cebfc6799909f4ecd71777bb603c169b5ad
jeonggeon8419 <geon8419@naver.com>
2023-09-15 16:50:13 +0900

    test0915.php

--- test/test.php
+++ test/test.php
@@ -12,6 +12,54 @@ for ($i = 2; $i <= 9; $i++) {
 	}
 }
 
-//숫자 맞추기 게임
-// 1. 1~100의 랜덤한 숫자를 맞추는 게임입니다
+// 1~10까지 출력
+for($i = 1; $i <= 10; $i++) {
+    echo "$i"."\n";
+}
+$mul = 1;
+while($mul <= 10) {
+	echo "$mul\n";
+
+	$mul++;
+}
+
+// 8단 출력
+// for($i = 1; $i <= 9; $i++) {
+// 	$pal = $i * 8;
+// 	echo "8 x {$i} = {$pal} \n";
+// }
+
+$i = 1;
+
+while(true) {
+	$pal = 7 * $i;
+	echo "7 x {$i} = {$pal}"."\n";
+	if($i === 9) {
+		break;
+	}
+	$i++;
+}
+
+// 19단 출력
+// for($i = 1; $i <= 9; $i++) {
+// 	$pal = $i * 19;
+// 	echo "19 x {$i} = {$pal}"."\n";
+// }
+
+//두 숫자 더해주는 함수
+function me_sum($a, $b) {
+	echo $a + $b;
+}
+
+echo me_sum(1,5)."\n";
+
+$zz = "짜장면";
+
+if($zz === "짜장면") {
+	echo "중식";
+} elseif ($zz === "비빔밥") {
+	echo "한식";
+} else {
+	echo "양식";
+}
 
