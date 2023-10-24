8884a69b5ac3d134bbfb41274b6e3f94c4e43b40
jeonggeon8419 <geon8419@naver.com>
2023-10-24 10:32:30 +0900

    js 10/24

--- javascript/js/04.js
+++ javascript/js/04.js
@@ -19,3 +19,36 @@ console.log(u_name);
 const AGE = 19;
 // AGE = 20;
 console.log(AGE);
+
+//------
+// 스코프
+//------
+// 전역 스코프
+let gender = "M";
+
+// 함수레벨 스코프
+function test() {
+	let test1 = "test1";
+	if(true) {
+		 test1 = "test2";
+	}
+	console.log(test1);
+}
+
+
+// 블록레벨 스코프
+function test2() {
+	var t = "test1"
+	if(true) {
+		 var t  = "test2";
+	}
+	console.log(t);
+}
+
+function test3() {
+	let t = "test1";
+	if(true) {
+		 let t = "test2";
+	}
+	console.log(t);
+}
