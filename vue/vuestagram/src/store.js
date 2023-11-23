315314926b608899069bb6ac562f63987a04fc3b
jeonggeon8419 <geon8419@naver.com>
2023-11-23 09:36:39 +0900

    vue 11/23

---
+++ vue/vuestagram/src/store.js
@@ -0,0 +1,107 @@
+import { createStore } from 'vuex';
+import axios from 'axios';
+
+const store = createStore({
+	// state() : data를 저장하는 영역
+	state() {
+		return {
+			//게시글을 저장용
+			boardData: [],
+			// 탬 ui용 flg 
+			flgTapUI: 0,
+			// 작성탭 표시용 이미지 URL 저장용 
+			imgURL: '',
+			// 글 작성용 파일데이터 저장
+			postFileData: null,
+			// 가장 마지막 로드 된 게시글 번호 저장용
+			lastBoardId: 0,
+		}
+	},
+
+	// mutations : 데이터 수정용 함수 저장 영역
+	mutations: {
+		// 초기 데이터 셋팅용 뮤테이션 쓸때는 state
+		setBoardList(state, data) {
+			state.boardData = data;
+			state.lastBoardId = data[data.length - 1].id;
+		},
+		// 탭 ui 셋팅용
+		setFlgTapUI(state, num) {
+			state.flgTapUI = num;
+		},
+		// 작성탭 표시용 이미지 URL 저장용
+		setImgURL(state, url) {
+			state.imgURL = url;
+		},
+		// 글 작성용 파일데이터 저장
+		setPostFileData(state, file) {
+			state.postFileData = file;
+		},
+		// 작성 된 글 삽입용
+		setUnshiftBoard(state, data) {
+			state.boardData.unshift(data);
+		},
+		// 작성 후 초기화 처리
+		setClearAddData(state) {
+			state.imgURL = '';
+			state.postFileData = null;
+		},
+	
+	},
+	// actions : ajax로 서버에 데이터를 요청할 때나  시간 함수등 비동기 처리는 actions에 정의
+	actions: {
+		//초기 게시글 데이터 획득 ajax처리 액션즈를 슬떄는 context
+		actionGetBoardList(context) {
+			const url = 'http://112.222.157.156:6006/api/boards';
+			const header = {
+				headers: {
+					'Authorization': 'Bearer meerkat'
+				}
+			};
+			axios.get(url, header)
+			.then(res => {
+				// console.log(res.status);
+				// console.log(res.data);
+				context.commit('setBoardList', res.data);
+
+				
+			})
+			.catch(err => {
+				console.log(err);
+			})
+		},
+		// 글 작성 처리
+		actionPostBoardAdd(context) {
+			const url = 'http://112.222.157.156:6006/api/boards';
+			const header = {
+				headers: {
+					'Authorization': 'Bearer meerkat',
+					'Content-Type' : 'multipart/form-data',
+				}
+			};
+			const data = {
+				name: '정건',
+				img: context.state.postFileData,
+				content: document.getElementById('content').value,
+			};
+
+			axios.post(url, data, header)
+			.then(res => {
+				// 작성글 데이터 저장
+				context.commit('setUnshiftBoard', res.data);
+
+				// 리스트 UI로 전환
+				context.commit('setFlgTapUI', 0);
+
+				// 작성후 초기화
+				context.commit('setClearAddData');
+			})
+			.catch(err => {
+				console.log(err);
+			});
+		},
+	
+	}
+});
+
+export default store;
